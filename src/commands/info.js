fdfc83086bcb255956d3ba72e9b74ba398bb5628
Jaid <jaid.jsx@gmail.com>
2020-12-11 03:49:33 +0100

    Improved `info` command
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/commands/info.js
+++ src/commands/info.js
@@ -1,22 +1,34 @@
 import execa from "execa"
+import matchArray from "match-array"
 
 import logger from "lib/logger"
 
 const tools = {
   ffmpeg: {
-    extractVersion: /version (?<version>.*?) /,
+    extractVersion: /version (?<version>.+?) /,
     commandArguments: ["-version"],
   },
   ffprobe: {
     commandArguments: ["-version"],
-    extractVersion: /version (?<version>.*?) /,
+    extractVersion: /version (?<version>.+?) /,
   },
   youtubeDl: {
     commandArguments: ["--version"],
   },
   autosub: {
     commandArguments: ["--version"],
-    extractVersion: /autosub (?<version>.*?) /,
+    extractVersion: /autosub (?<version>.+?) /,
+    bonusHandler: async argv => {
+      const listLanguagesResult = await execa(argv.autosubPath, ["--list-speech-codes"])
+      const extractCodesResult = matchArray(/\n(?<code>[a-z]+-[a-z-]+)/g, listLanguagesResult.stdout)
+      const listFormatsResult = await execa(argv.autosubPath, ["--list-formats"])
+      const outputFormats = /(?<content>.*)input format/s.exec(listFormatsResult.stdout).groups.content
+      const extractOutputFormatsResult = matchArray(/\n(?<code>[.a-z]+)/g, outputFormats)
+      return {
+        Languages: extractCodesResult.map(entry => entry.code).join(", "),
+        Formats: extractOutputFormatsResult.map(entry => entry.code).join(", "),
+      }
+    },
   },
 }
 
@@ -38,6 +50,12 @@ export default async argv => {
       logger.info(`| Version: ${regexResult.groups.version}`)
     } else {
       logger.info(`| Version: ${result.stdout}`)
+    }
+    if (properties.bonusHandler) {
+      const handlerResult = await properties.bonusHandler(argv, key, properties)
+      for (const [bonusKey, bonusValue] of Object.entries(handlerResult)) {
+        logger.info(`| ${bonusKey}: ${bonusValue}`)
+      }
     }
   }
 }
